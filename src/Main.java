0398d4530147367d7f487e6cb5eb9ee5f1987bba
Michail <ilims@mail.ru>
2022-08-17 01:18:49 +0700

    Дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        int i = 0;
+        while (i < 10){
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i--){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Задача 2
+        for (int fridat = 5; fridat <= 31; fridat += 7) {
+            System.out.println("Сегодня пятница, " + fridat + "-e число. Необходимо подготовить отчет. ");
+        }
+
+        // Задача 3
+
+        int currentYear = 2022;
+        int start = currentYear - 200;
+        int ent = currentYear + 100;
+        int period = 79;
+        int firstAppearedYear = 0;
+        for (int year = start; year <= ent; year++){
+            if (year % period == firstAppearedYear){
+                System.out.println(year);
+            }
+        }
+
+
+
     }
 }
